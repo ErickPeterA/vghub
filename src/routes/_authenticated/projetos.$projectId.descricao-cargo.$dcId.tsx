d0a67e79c8eff3e69915b88148ba82723b8123d6
ErickPeterA <erickpeterdearaujo@gmail.com>
2026-07-07 14:55:08 -0300

    update supabase

--- src/routes/_authenticated/projetos.$projectId.descricao-cargo.$dcId.tsx
+++ src/routes/_authenticated/projetos.$projectId.descricao-cargo.$dcId.tsx
@@ -155,7 +155,7 @@ function EditDC() {
         onSubmit={onSubmit}
         submitLabel="Salvar alterações"
         readOnly={readOnly}
-        commentTarget={{ dcId, versionId: versionIdForComments, canAddComment: !viewingVersionId }}
+        commentTarget={{ dcId, versionId: versionIdForComments, canAddComment: (isLider || isAdmin) && !viewingVersionId }}
         footerExtra={showLiderActions ? (
           <button
             type="button"
